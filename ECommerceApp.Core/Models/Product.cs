add67140759a1b7bceb69520e13170222b031692
maksim <m>
2025-09-26 13:34:15 +0300

    Реализована логика

---
+++ ECommerceApp.Core/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Core.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }          // уникальный идентификатор
+        public string Name { get; set; }     // название
+        public decimal Price { get; set; }   // цена
+
+        public Product(int id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}: {Name} - {Price}₽";
+        }
+    }
+}
---
+++ ECommerceApp.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using ECommerceApp.Core.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ECommerceApp.Data.Repositories
+{
+    public class ProductRepository
+    {
+        private readonly List<Product> _products = new();
+
+        public void AddProduct(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            return _products;
+        }
+
+        public Product? GetById(int id)
+        {
+            return _products.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
